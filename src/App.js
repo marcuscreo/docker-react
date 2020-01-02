be0acd1a849beefae5b2e17d59b95e16f6b752e1
marcuscreo <marcus@creoagency.com>
2020-01-02 13:21:18 -0800

    build, test, and prod stuff

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          CHANGE 6:HI MARCUS: Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
